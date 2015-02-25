36c59ea45773d1c63a5b595f956317dc67705c3f
evoup <evoex123@gmail.com>
2015-02-25 17:52:59 +0800

    c

---
+++ FMP_api/src/main/java/com/madhouse/spring/common/HttpUtil.java
@@ -0,0 +1,52 @@
+package com.madhouse.spring.common;
+
+import org.apache.commons.httpclient.HostConfiguration;
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.HttpMethod;
+import org.apache.commons.httpclient.URI;
+import org.apache.commons.httpclient.methods.GetMethod;
+
+
+public class HttpUtil {
+    /**
+     * 模拟一个get请求
+     */
+	public String doGet(String http_url, boolean isProxy) {
+	    HttpMethod method = null;
+	    try {
+	        URI uri = new URI(http_url, true);
+	        HttpClient client = new HttpClient();
+	        HostConfiguration hcfg = new HostConfiguration();
+	        hcfg.setHost(uri);
+	        client.setHostConfiguration(hcfg);
+	        //if (isProxy)
+	        //    setProxy(client);
+	        // 参数验证
+	        client.getParams().setAuthenticationPreemptive(true);
+	        // GET请求方式
+	        method = new GetMethod(http_url);
+	        client.executeMethod(method);
+	        System.out.println("state:" + method.getStatusLine());
+	        //System.out.println("Qs:" + method.getQueryString());
+	        //System.out.println("response body:" + method.getResponseBodyAsString());
+	        return method.getResponseBodyAsString();
+	    } catch (Exception e) {
+	        e.printStackTrace();
+	    }
+	    return null;
+	}
+	
+    /**
+     * 设置代理
+     */
+    //private void setProxy(HttpClient client) {
+        // 设置代理
+        //client.getHostConfiguration().setProxy("172.16.30.222", 8119);
+        //client.getHostConfiguration().setProxy(HTTP_HOST, HTTP_PORT);
+        //client.getState().setProxyCredentials(AuthScope.ANY,
+        //new UsernamePasswordCredentials(HTTP_USER, HTTP_PWD));
+    //}
+
+}
+
+
