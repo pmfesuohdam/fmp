f4b0a68ea2b31b3cde59193d098142abf7c599ce
evoup <evoex123@gmail.com>
2015-03-01 21:11:02 +0800

    c

--- FMP_ui/assets/js/base.js
+++ FMP_ui/assets/js/base.js
@@ -3,6 +3,6 @@ var baseConf={
 	"api_prefix":"http://"+document.domain+"/fmpapi1.0",
 	"client_id":"780285318715815",
 	"logged_gourl":"http://"+document.domain+"/settings",
-	"redirect_url":"http://"+document.domain+"/redirect.html",
+	"redirect_url":"http://"+document.domain+"/redirect/",
 	"fb_login_url":"https://www.facebook.com/v2.2/dialog/oauth?client_id=780285318715815&redirect_uri=http://"+document.domain+"/fb_login/index.html&scope=offline_access&manage_pages&ads_management&read_insights&publish_actions"
 }
