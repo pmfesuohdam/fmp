028710e42b68c8d485ab2a3abd1ece4613c74bed
evoup <evoex123@gmail.com>
2015-03-02 01:40:52 +0800

    业务错误代码

--- FMP_api_php/fun/fb_loginFun.m
+++ FMP_api_php/fun/fb_loginFun.m
@@ -34,15 +34,15 @@ if($GLOBALS['selector'] == __SELECTOR_SINGLE) {
             //格式正确,再检查是否拥有广告账号
             $ret=file_get_contents('https://graph.facebook.com/v2.2/me/businesses?access_token='.$token);
             $ret=json_decode($ret,true);
-            print_r($ret);
+            //print_r($ret);
             //存所有获取adaccount的数组
-            $getAdAccuntsArr=null;
+            $getAdAccuntsArr=$getAdAccuntsNameArr=null;
             if ( isset($ret['data']) && !empty($ret['data']) ) {
                 foreach ($ret['data'] as $businessDetail) {
-                    echo "[business][name:{$businessDetail['name']}][id:{$businessDetail['id']}]";
+                    //echo "[business][name:{$businessDetail['name']}][id:{$businessDetail['id']}]";
                     //捞到business的id之后，方可获取广告账号信息
                     $try4getadaccount=0;
-                    $adaccounts_url='https://graph.facebook.com/v2.2/'.$businessDetail['id'].'?fields=adaccounts&access_token='.$token;
+                    $adaccounts_url='https://graph.facebook.com/v2.2/'.$businessDetail['id'].'?fields=adaccounts,name&access_token='.$token;
                     while ( $try4getadaccount<3 && !(isset($ret1['adaccounts']) && !empty($ret1['adaccounts'])) ) {
                         $ret1=json_decode(strval(file_get_contents($adaccounts_url)),true);
                         //print_r($ret1);
@@ -54,7 +54,8 @@ if($GLOBALS['selector'] == __SELECTOR_SINGLE) {
                         foreach ($ret1['adaccounts']['data'] as $adaccountDetail) {
                             echo "<pre>";
                             //print_r($adaccountDetail);
-                            $getAdAccuntsArr[$adaccountDetail['account_id']]=json_encode($adaccountDetail);
+                            $getAdAccuntsArr[$adaccountDetail['account_id']]=$adaccountDetail;
+                            $getAdAccuntsNameArr[$adaccountDetail['account_id']]=$ret1['name'];
                         }
                     } else {
                         $msgs['business']='no ad account found under your facebook account!';
@@ -64,30 +65,37 @@ if($GLOBALS['selector'] == __SELECTOR_SINGLE) {
             } else {
                 $msgs['business']='no business found under your facebook account!';
             }
-            print_r($getAdAccuntsArr);
+            //print_r($getAdAccuntsArr);
             $now=time();
-            $insert_detail=addslashes($getAdAccuntsArr[$adaccountDetail['account_id']]);
-            $query=<<<EOT
+            foreach ($getAdAccuntsArr as $adaccountDetail_id=>$adaccountDetail2) {
+                $insert_detail=addslashes(json_encode($adaccountDetail2));
+                $query=<<<EOT
             INSERT INTO `t_fb_account` (ad_account_name,ad_account_id,access_token,ad_account_detail) 
-                VALUES ("734152.979166667","{$adaccountDetail['account_id']}","{$token}","{$insert_detail}")
+                VALUES ("{$getAdAccuntsNameArr[$adaccountDetail2['account_id']]}","{$adaccountDetail2['account_id']}","{$token}","{$insert_detail}")
                 ON DUPLICATE KEY UPDATE 
-                  ad_account_name="734152.979166668",access_token="{$token}",ad_account_detail="{$insert_detail}",
+                  ad_account_name="{$getAdAccuntsNameArr[$adaccountDetail2['account_id']]}",access_token="{$token}",ad_account_detail="{$insert_detail}",
                   update_time=now();
 EOT;
-            echo $query;
-            include(dirname(__FILE__).'/../inc/conn.php');
-            if (!$link->query($query)) {
-                $msgs['err_msg']='Sorry, something we are disturbed.';
-                $msgs['status']='false';
-                break;
-            }
-
-            //保存广告账号
-            //$link= mysqli_init();
-            //$link->options(MYSQLI_OPT_CONNECT_TIMEOUT, 8);
-            //$link->real_connect(__DB_MYSQL_HOST, __DB_MYSQL_USER, __DB_MYSQL_PASS, __DB_MYSQL_DB);
-            //$link->query("SET NAMES utf8");
-            //$query="";
+                //echo $query;
+                include(dirname(__FILE__).'/../inc/conn.php');
+                if (!$link->query($query)) {
+                    $msgs['err_msg']='Sorry, something we are disturbed.('.__FMP_ERR_UPDATE_ADACCOUNT.')';
+                    $msgs['status']='false';
+                    break;
+                } else { //保存账号所属fmp用户关系 
+                    $query=<<<EOT
+INSERT INTO `t_relationship_fmp_fb` (fmp_user_id,fb_adaccount_id)
+    VALUES ("10001","{$adaccountDetail2['account_id']}") 
+    ON DUPLICATE KEY UPDATE 
+      update_time=now();
+EOT;
+                    if (!$link->query($query)) {
+                        $msgs['err_msg']='Sorry, something we are disturbed.('.__FMP_ERR_UPDATE_FMP_FB_REL.')';
+                        $msgs['status']='false';
+                        break;
+                    }
+                }
+            } //end foreach
 
         } else $msgs['status']='false';
         echo json_encode($msgs);
