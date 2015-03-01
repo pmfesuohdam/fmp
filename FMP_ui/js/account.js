6df96278d0a72e191d79f958daa26c1a8a25ee2e
evoup <evoex123@gmail.com>
2015-03-01 20:38:50 +0800

    c

--- FMP_ui/js/account.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-	$("#add_fb_a").attr("href",baseConf.fb_login_url)
-})
-function doDelAdAccount(act_id) {
-  document.getElementById("hid_del_act").value=act_id;
-  document.getElementById("do_account_form").submit();
-  }
