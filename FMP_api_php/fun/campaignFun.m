98f4a28653a7bf9582184ca780eaf99f21a80de3
evoup <evoex123@gmail.com>
2015-03-04 17:18:17 +0800

    c

--- FMP_api_php/fun/campaignFun.m
+++ FMP_api_php/fun/campaignFun.m
@@ -9,15 +9,39 @@
   +----------------------------------------------------------------------+
   | Create:
   +----------------------------------------------------------------------+
-  | Last-Modified: 2015-03-03 14:53:02
+  | Last-Modified: 2015-03-04 17:02:24
   +----------------------------------------------------------------------+
-*/
+ */
 $GLOBALS['httpStatus'] = __HTTPSTATUS_BAD_REQUEST; //默认返回400 
 header("Content-type: application/json; charset=utf-8");
 if ($GLOBALS['selector'] == __SELECTOR_SINGLE) {
-    $ret=null;
-    $ret['status']="true";
-    $GLOBALS['httpStatus']=__HTTPSTATUS_OK;
-    echo json_encode($ret);
+    switch($GLOBALS['operation']) {
+    case(__OPERATION_READ):
+        if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+            $ret=null;
+            $ret['status']="true";
+            $GLOBALS['httpStatus']=__HTTPSTATUS_OK;
+            echo json_encode($ret);
+        }
+        break;
+    }
+} 
+
+//TODO 做完第一步就可以分一分文件了
+//发布广告第一步
+if ($GLOBALS['selector'] == __SELECTOR_STEP1) {
+    switch($GLOBALS['operation']) {
+    case(__OPERATION_READ): //发送当前保存的数据 
+        if ($_SERVER['REQUEST_METHOD']=='GET'){
+            $GLOBALS['httpStatus']=__HTTPSTATUS_OK;
+            break;
+        }
+    case(__OPERATION_UPDATE): //接受提交数据 
+        if ($_SERVER['REQUEST_METHOD']=='POST'){
+            $GLOBALS['httpStatus']=__HTTPSTATUS_OK;
+            break;
+        }
+    }
 }
+
 ?>
