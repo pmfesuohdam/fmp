519815e78e812f25df6f016590c29011e2913f21
evoup <evoex123@gmail.com>
2015-03-19 10:32:44 +0800

    c

--- FMP_ui/assets/js/fmpTabs.js
+++ FMP_ui/assets/js/fmpTabs.js
@@ -1,42 +1,101 @@
-(function($) {
-  $.fn.fmpTabs = function(options) {
-      var opts = $.extend({}, $.fn.fmpTabs.defaults, options);
-      $this = $(this);
-      var o = $.meta ? $.extend({}, opts, $this.data()) : opts;
-//      $this.css({
-//        backgroundColor: o.background,
-//        color: o.foreground
-//      });
-//      var markup = $this.html();
-//      markup = $.fn.fmpTabs.format(markup);
-//      $this.html(markup);
-    
-//#dialog label, #dialog input { display:block; }
-//#dialog label { margin-top: 0.5em; }
-//#dialog input, #dialog textarea { width: 95%; }
-//#tabs { margin-top: 1em; }
-//#tabs li .ui-icon-close { float: left; margin: 0.4em 0.2em 0 0; cursor: pointer; }
-//#add_tab { cursor: pointer; }
-     $this
-         .html("")
-         .append('<ul><li><a href="#tabs-1">Nunc tincidunt</a><span class="ui-icon ui-icon-close" role="presentation">Remove Tab</span></li></ul><div id="tabs-1"><p></p></div>')
-         .before('<div id="dialog" title="Tab data"><form onsubmit="return false;"><fieldset class="ui-helper-reset"><label for="tab_title">Title</label><input type="text" name="tab_title" id="tab_title" value="Tab Title" class="ui-widget-content ui-corner-all"><label for="tab_content">Content</label><textarea name="tab_content" id="tab_content" class="ui-widget-content ui-corner-all">Tab content</textarea></fieldset></form></div><button id="add_tab">Add Tab</button>')
-  };
-  $("#dialog label, #dialog input").css("display","block")
-  $("#dialog label").css("margin-top","0.5em")
-  $("#dialog input, #dialog textarea").css("width","95%")
-  $("#tabs").css("margin-top","1em")
-  $("#tabs li .ui-icon-close").css({float:"left",margin:"0.4em 0.2em 0 0",cursor:"pointer"})
-  $("add_tab").css("cursor","pointer")
+//示例插件
+(function ($) {
+    $.fn.fmp_tab = function (options) {
+    // opts 里面是默认参数
+    var opts={ 
+        color:'red',
+        add_name:'Add Tab',
+        add_tab_name:'Tab data',
+        callback:function(){console.log(opts.color)}
+    };
+    $.extend(opts,options);
 
-  $.fn.fmpTabs.format = function(txt) {
-    return '<strong>' + txt + '</strong>';
-  };
-  //
-  // plugin defaults
-  //
-  $.fn.fmpTabs.defaults = {
-    foreground: 'red',
-    background: 'yellow'
-  };
+// jquery功能代码
+//this.children("p").css({"color":opts.color});
+myid=$(this).eq(0).attr('id');
+
+//$(this).css({"color":"#00C"});
+
+
+this.before('<div id="dialog" title="'+opts.add_tab_name+'"><form><fieldset class="ui-helper-reset"><label for="tab_title">Title</label><input type="text" name="tab_title" id="tab_title" value="Tab Title" class="ui-widget-content ui-corner-all"><label for="tab_content">Content</label><textarea name="tab_content" id="tab_content" class="ui-widget-content ui-corner-all">Tab content</textarea></fieldset></form></div><button id="add_tab">'+opts.add_name+'</button>')
+//list=this.append('<ul></ul>').find('ul');
+this.append('<ul><li><a href="#'+myid+'-1">product1</a> <span class="ui-icon ui-icon-close" role="presentation">Remove Tab</span></li></ul><div id="'+myid+'-1"><p>content</p></div>');
+//////////////////////////////////////
+var tabTitle = $( "#tab_title" ),
+tabContent = $( "#tab_content" ),
+tabTemplate = "<li><a href='#{href}'>#{label}</a> <span class='ui-icon ui-icon-close' role='presentation'>Remove Tab</span></li>",
+tabCounter = 2;
+
+var tabs = $( "#"+myid ).tabs();
+
+// modal dialog init: custom buttons and a "close" callback resetting the form inside
+var dialog = $( "#dialog" ).dialog({
+    autoOpen: false,
+    modal: true,
+    buttons: {
+        Add: function() {
+            addTab();
+            $( this ).dialog( "close" );
+        },
+        Cancel: function() {
+            $( this ).dialog( "close" );
+        }
+    },
+    close: function() {
+        form[ 0 ].reset();
+    }
+});
+
+// addTab form: calls addTab function on submit and closes the dialog
+var form = dialog.find( "form" ).submit(function( event ) {
+    addTab();
+    dialog.dialog( "close" );
+    event.preventDefault();
+});
+
+// actual addTab function: adds new tab using the input from the form above
+function addTab() {
+    var label = tabTitle.val() || "Tab " + tabCounter,
+    id = myid+"-" + tabCounter,
+    li = $( tabTemplate.replace( /#\{href\}/g, "#" + id ).replace( /#\{label\}/g, label ) ),
+    tabContentHtml = tabContent.val() || "Tab " + tabCounter + " content.";
+
+    tabs.find( ".ui-tabs-nav" ).append( li );
+    tabs.append( "<div id='" + id + "'><p>" + tabContentHtml + "</p></div>" );
+    tabs.tabs( "refresh" );
+    tabCounter++;
+}
+
+// addTab button: just opens the dialog
+$( "#add_tab" )
+.button()
+.click(function() {
+    dialog.dialog( "open" );
+});
+
+// close icon: removing the tab on click
+tabs.delegate( "span.ui-icon-close", "click", function() {
+    var panelId = $( this ).closest( "li" ).remove().attr( "aria-controls" );
+    $( "#" + panelId ).remove();
+    tabs.tabs( "refresh" );
+});
+
+tabs.bind( "keyup", function( event ) {
+    if ( event.altKey && event.keyCode === $.ui.keyCode.BACKSPACE ) {
+        var panelId = tabs.find( ".ui-tabs-active" ).remove().attr( "aria-controls" );
+        $( "#" + panelId ).remove();
+        tabs.tabs( "refresh" );
+    }
+});
+
+// css
+$("#dialog label, #dialog input").css({"display":"block","font-size":"12px"})
+$("#dialog label").css("margin-top"," 0.5em")
+$("#dialog input, #dialog textarea").css("width","95%")
+$("#tabs").css("margin-top","1em")
+$("#tabs li .ui-icon-close").css({float:"left",margin:"0.4em 0.2em 0 0",cursor:"pointer"})
+$("#add_tab").css("cursor","pointer")
+/////////////////////////////////////
+opts.callback();
+};
 })(jQuery);
