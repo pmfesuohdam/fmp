0afb406b8f03a6675e443fc1e37ae9a16b1e3bc6
evoup <evoex123@gmail.com>
2015-03-23 14:03:45 +0800

    c
    alert message支持ok和fail

--- FMP_ui/assets/js/fmpAlertMessage.js
+++ FMP_ui/assets/js/fmpAlertMessage.js
@@ -2,16 +2,25 @@
     $.fn.fmp_alert_message = function (options) {
     // opts 里面是默认参数
     var opts={ 
-        title:'dialog_title',
-        content:'dialog_content',
-        height:180,
+        type:1,
+        strong:'alert!',
+        content:'alert content',
         callback:function(){}
     };
     $.extend(opts,options);
 
+    this.empty()
+    appendStr=""
+    switch(opts.type) {
+      case(0):
+      appendStr="<div class=\"alert alert-danger alert-error\">";
+      break;
+      case(1):
+      appendStr="<div class=\"alert alert-success\">";
+      break;
+    }
     this
-        .empty()
-        .append("<div class=\"alert alert-success\"><a href=\"#\" class=\"close\" data-dismiss=\"alert\">&times;</a><strong>Success!</strong> We are deleting the selected account, it could take some time.</div>")
+        .append(appendStr+"<a href=\"#\" class=\"close\" data-dismiss=\"alert\">&times;</a><strong>"+opts.strong+"</strong> "+opts.content+"</div>")
         .css("display","none")
         .fadeIn()
     opts.callback();
