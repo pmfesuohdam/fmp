2992a03924caac42d0d314ff90c264ffbcbe09e0
evoup <evoex123@gmail.com>
2015-03-23 14:56:57 +0800

    c
    sync request ok

--- FMP_api_php/fun/syncFun.m
+++ FMP_api_php/fun/syncFun.m
@@ -19,8 +19,32 @@ if($GLOBALS['selector'] == __SELECTOR_SINGLE) {
         switch($GLOBALS['operation']) {
         case(__OPERATION_READ):
             //adaccount必须存在且属于当前用户
-            $GLOBALS['httpStatus']=__HTTPSTATUS_OK;
-            $ret['status']='true';
+            if (empty($_GET['sync_adaccount'])) {
+                $msgs['err_msg'][]=array('sync_adaccount'=>'param sync_adaccount must not be empty');
+            } else {
+                include(dirname(__FILE__).'/../inc/conn.php');
+                $query="select t1.fb_adaccount_id,t2.ad_account_name from t_relationship_fmp_fb as t1 inner join t_fb_account as t2 where t1.fmp_user_id='{$_SESSION['fmp_uid']}' and t1.fb_adaccount_id=t2.ad_account_id;";
+                $rows=null;
+                if ($result=$link->query($query)) {
+                    while ($row=mysqli_fetch_assoc($result)) {
+                        $rows[]=$row['fb_adaccount_id'];
+                    }
+                }
+                if (!in_array($_GET['sync_adaccount'],$rows)) {
+                    $msgs['err_msg'][]=array('sync_adaccount'=>'the ad account is not belong to you');
+                }
+                $query2="update t_fb_account set want_sync=1 where ad_account_id={$_GET['sync_adaccount']} limit 1;";
+                if (!$link->query($query2)) {
+                    $msgs['err_msg'][]=array('sync_adaccount'=>'set sync flag fail');
+                }
+                @mysqli_close($link);
+            }
+            if ( !isset($msgs['err_msg']) || empty($msgs['err_msg']) ) {
+                $ret['status']='true';
+            } else {
+                $ret['status']='false';
+            }
+                $GLOBALS['httpStatus']=__HTTPSTATUS_OK;
             echo json_encode($ret);
             break;
         }
