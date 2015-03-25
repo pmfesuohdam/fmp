4d3f13021ab0c0b8ce7a95aeb271c3e06f96b8b6
evoup <evoex123@gmail.com>
2015-03-25 11:35:14 +0800

    c
    多产品的面版上有message和link

--- FMP_ui/assets/js/fmpTabs.js
+++ FMP_ui/assets/js/fmpTabs.js
@@ -50,7 +50,7 @@ function addTab() {
     var label = tabTitle.val() || "Product " + tabCounter,
     id = myid+"-" + tabCounter,
     li = $( tabTemplate.replace( /#\{href\}/g, "#" + id ).replace( /#\{label\}/g, label ) ),
-    tabContentHtml = tabContent.val() || "Product " + tabCounter + " content.";
+    tabContentHtml = tabContent.val() || "<div class=\"form-group\"><label for=\"product_link\">Product Link<code></code></label><input type=\"text\" class=\"form-control\" id=\"productLink\" name=\"productLink\" placeholder=\"Enter Link\" value=\"\"><label for=\"product_description\">Product Description<code></code></label><input type=\"text\" class=\"form-control\" id=\"productDesscription\" name=\"productDesscription\" placeholder=\"Enter Description\" value=\"\"></div>";
 
     tabs.find( ".ui-tabs-nav" ).append( li );
     tabs.append( "<div id='" + id + "'><p>" + tabContentHtml + "</p></div>" );
