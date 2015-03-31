a1159afea10a5b39c9c27a4f8d21aafb56092f3c
evoup <evoex123@gmail.com>
2015-03-31 17:59:36 +0800

    c
    adimages接口

--- FMP_api_php/fun/adimagesFun.m
+++ FMP_api_php/fun/adimagesFun.m
@@ -11,5 +11,59 @@
   +----------------------------------------------------------------------+
   | Last-Modified: 2015-03-31 15:41:20
   +----------------------------------------------------------------------+
-*/
+ */
+$GLOBALS['httpStatus'] = __HTTPSTATUS_BAD_REQUEST; //默认返回400 
+header("Content-type: application/json; charset=utf-8");
+if( in_array(
+    $GLOBALS['selector'], array(
+        __SELECTOR_PRODUCT1,
+        __SELECTOR_PRODUCT2,
+        __SELECTOR_PRODUCT3,
+        __SELECTOR_PRODUCT4,
+        __SELECTOR_PRODUCT5,
+        __SELECTOR_MASS
+    )
+)) {
+/*{{{如果是获取图片的路径*/
+    switch($GLOBALS['operation']) {
+    case(__OPERATION_READ):
+        if ($_SERVER['REQUEST_METHOD']=='GET'){
+            include(dirname(__FILE__).'/../inc/conn.php');
+            $query=<<<EOT
+SELECT a.content FROM t_fmp_material a INNER JOIN t_fmp_user_material b 
+    WHERE b.fmp_user_id{$_SESSION[__SESSION_FMP_UID]}
+    AND a.fmp_hash=b.fmp_material_hash;
+EOT;
+            $result=$link->query($query);
+            if ( !($row = mysqli_fetch_assoc($result)) ) {
+                $msgs['err_msg'][]=array('billingAccount'=>'billingAccount not exists');
+                @mysqli_close($link);
+            }
+            $GLOBALS['httpStatus']=__HTTPSTATUS_OK;
+        }
+        break;
+    }
+/*}}}*/
+} else {
+/*{{{如果是根据url获取图片*/
+    $arrPathInfo=parse_url($_SERVER['REQUEST_URI']);
+    $qs=$arrPathInfo['path'];
+    $qs=explode('/',$arrPathInfo['path']);
+    $fileName=array_pop($qs);
+    $info=pathinfo($fileName);
+    $hash=basename($fileName,".{$info['extension']}");
+    $tail=array_pop($qs);
+    $head=array_pop($qs);
+    if (strlen($hash)!=32) {
+        //不是md5 32位啊
+    } elseif(!in_array(strtolower($info['extension']),array('gif','png','jpg','jpeg'))) {
+        //扩展名不对吧
+    } elseif(GetMaterialPath($hash)!="{$head}/{$tail}") {
+        //路径不对吧
+    } else {
+        //出图
+        $GLOBALS['httpStatus']=__HTTPSTATUS_OK;
+    }
+/*}}}*/
+}
 ?>
