8b4c24f9ff768d5d9eaa3bd8bc2705a29208003f
evoup <evoex123@gmail.com>
2015-03-31 23:56:15 +0800

    c
    server side js combine

---
+++ FMP_api_php/fun/js_minFun.m
@@ -0,0 +1,29 @@
+<?php
+/*
+  +----------------------------------------------------------------------+
+  | Name:js_minFun.m
+  +----------------------------------------------------------------------+
+  | Comment:server端合并js的函数
+  +----------------------------------------------------------------------+
+  | Author:Evoup     evoex@126.com                                                     
+  +----------------------------------------------------------------------+
+  | Create:
+  +----------------------------------------------------------------------+
+  | Last-Modified: 2015-03-31 23:19:24
+  +----------------------------------------------------------------------+
+ */
+switch($GLOBALS['operation']) {
+case(__OPERATION_READ):
+    if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+        $jsFiles=explode(',',$_GET['f']);
+        foreach($jsFiles as $jsFile) {
+            if (file_exists(__WEBUI_ROOT."/{$jsFile}")){
+                $minJs.="/*!\n* COMBINE:{$jsFile}\n*/\n".file_get_contents(__WEBUI_ROOT."/{$jsFile}");
+            }
+        }
+        echo $minJs;
+        $GLOBALS['httpStatus']=__HTTPSTATUS_OK;
+    }
+    break;
+}
+?>
