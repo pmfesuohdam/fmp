fc595d9293ff6976651c363659eef4c4ce31c53d
evoup <evoex123@gmail.com>
2015-04-05 04:24:56 -0700

    delete nouse code

--- FMP_api_php/fun/fb_loginFun.m
+++ FMP_api_php/fun/fb_loginFun.m
@@ -9,7 +9,7 @@
   +----------------------------------------------------------------------+
   | Create:
   +----------------------------------------------------------------------+
-  | Last-Modified: 2015-03-03 10:40:04
+  | Last-Modified: 2015-04-05 04:24:27
   +----------------------------------------------------------------------+
  */
 $GLOBALS['httpStatus'] = __HTTPSTATUS_BAD_REQUEST; //默认返回400 
@@ -53,7 +53,6 @@ if($GLOBALS['selector'] == __SELECTOR_SINGLE) {
                         '/userpermissions?fields=adaccount_permissions&user='.$_SESSION[__SESSION_FB_UID].'&access_token='.$token;
                     while ( $try4getadaccount<3 && !(isset($ret1['data'][0]) && !empty($ret1['data'][0])) ) {
                         $ret1=json_decode(strval(file_get_contents($adaccounts_url)),true);
-                        //print_r($ret1['data'][0]);
                         usleep(2000);
                         $try4getadaccount++;
                     }
@@ -71,8 +70,6 @@ if($GLOBALS['selector'] == __SELECTOR_SINGLE) {
                     }
                     unset($try4getadaccount,$ret1);
                 }
-                //print_r($getAdAccuntsArr);
-                //print_r($getAdAccuntsNameArr);
                 if (!$GLOBALS['got_ad_account']) { //一个也没有 
                     $msgs['err_msg'][]=Array('business'=>'no ad account found under your facebook account!');
                 }
@@ -96,7 +93,6 @@ EOT;
                     @mysqli_close($link);
                     break;
                 } else { //保存账号所属fmp用户关系 
-                    //print_r($adaccountDetail2);
                     $adaccountDetail2['id']=str_replace('act_','',$adaccountDetail2['id']);
                     $query=<<<EOT
 INSERT INTO `t_relationship_fbaccount` (fmp_user_id,fb_adaccount_id)
@@ -116,64 +112,7 @@ EOT;
         @mysqli_close($link);
         echo json_encode($msgs);
         $GLOBALS['httpStatus'] = __HTTPSTATUS_OK;
-        break;
         /* }}} */
-        /* {{{ 登录处理
-         */
-        $msgs['err_msg']=array(); //返回的消息
-        $err_item=array();
-        //已经登录的
-        //{"status":"true"};
-        //如果有问题的话
-        //{"err_msg":[{"字段名":"错误消息"},{"字段名":"错误消息"}],"status":"false"}
-        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-            $row=null;
-            //email需要满足格式，长度最少6，最大50,不为空，数据库中有
-            $err_item=null;
-            if (empty($_POST['email'])) {
-                $err_item['email']='email must be not empty';
-                $msgs['err_msg'][]=$err_item;
-            } else if (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
-                $err_item['email']='email format err';
-                $msgs['err_msg'][]=$err_item;
-            } else if (strlen($_POST['email'])<6 || strlen($_POST['email'])>50 ) {
-                $err_item['email']='email size must between 6 and 50';
-                $msgs['err_msg'][]=$err_item;
-            } else {
-                include(dirname(__FILE__).'/../inc/conn.php');
-                $query="select name,passwd from `".__TB_FMP_USER."` where email='".$_POST['email']."';";
-                $result=$link->query($query);
-                if ( !($row = mysqli_fetch_assoc($result)) ) {
-                    $err_item['email']='user not exists';
-                    $msgs['err_msg'][]=$err_item;
-                }
-                @mysqli_close($link);
-            }
-
-            //password需要满足长度最少6，最大20,不为空,如果用户民存在要检查密码匹配
-            $err_item=null;
-            if (empty($_POST['passwd'])) {
-                $err_item['passwd']='password must be not empty';
-                $msgs['err_msg'][]=$err_item;
-            } else if (strlen($_POST['passwd'])<6 || strlen($_POST['passwd'])>20 ) {
-                $err_item['passwd']='password size must between 6 and 20';
-                $msgs['err_msg'][]=$err_item;
-            } else {
-                if ( !empty($row) ) {
-                    if (md5($_POST['passwd'])!=$row['passwd']) {
-                        $err_item['passwd']='passwd wrong';
-                        $msgs['err_msg'][]=$err_item;
-                    }
-                }
-            }
-        }
-        /* }}} */
-        if ( !isset($msgs['err_msg']) || empty($msgs['err_msg']) ) {
-            $msgs['status']="true";
-            $_SESSION['username']=$row['name'];
-        } else $msgs['status']="false";
-        $GLOBALS['httpStatus']=__HTTPSTATUS_OK;
-        echo json_encode($msgs);
         break;
     }
 }
