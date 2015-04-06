96b83923d84bd7312a268694dfe0e713ad7c726f
evoup <evoex123@gmail.com>
2015-04-06 23:36:17 +0800

    delete assets from minify

--- minify/assets/js/base.js
+++
@@ -1,8 +0,0 @@
-var baseConf={
-	"domain":"http://"+document.domain,
-	"api_prefix":"http://"+document.domain+"/fmpapi1.0",
-	"client_id":"798558363555177",
-	"logged_gourl":"http://"+document.domain+"/settings",
-	"redirect_url":"http://"+document.domain+"/redirect/",
-	"fb_login_url":"https://www.facebook.com/v2.2/dialog/oauth?client_id=798558363555177&redirect_uri=http://"+document.domain+"/fb_login/index.html&scope=offline_access,manage_pages,ads_management,read_insights,publish_actions"
-};
