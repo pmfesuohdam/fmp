2f258833e0035377f72bac56aa70294710dcac59
evoup <evoex123@gmail.com>
2015-04-14 14:19:00 +0800

    c

--- FMP_ui/assets/js/base.js
+++ FMP_ui/assets/js/base.js
@@ -19,7 +19,7 @@ $(document).ajaxComplete(function(event, xhr, settings) {
         // 请求API出现400或者明文出现status为false，则为没有登录，跳转到未登录页
         if (xhr.status===400 || login_status==="false" || login_status===false) {
           console.log("redirect")
-          //window.location.href=baseConf.redirect_url+"not_login.html"
+          window.location.href=baseConf.redirect_url+"not_login.html"
         }
     } catch(e) {}
 });
