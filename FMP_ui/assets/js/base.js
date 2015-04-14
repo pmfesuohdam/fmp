1ada766bdd81b011efa83af6258a3b4b5d27b083
evoup <evoex123@gmail.com>
2015-04-14 10:53:47 +0800

    c
    全局ajax触发退出

--- FMP_ui/assets/js/base.js
+++ FMP_ui/assets/js/base.js
@@ -1,9 +1,16 @@
 var baseConf = {
-	"domain": "http://"+document.domain,
-	"api_prefix": "http://"+document.domain+"/fmpapi1.0",
-	"client_id": "798558363555177",
-	"logged_gourl": "http://"+document.domain+"/settings",
-	"redirect_url": "http://"+document.domain+"/redirect/",
-	"fb_login_url": "https://www.facebook.com/v2.2/dialog/oauth?client_id=798558363555177&redirect_uri=http://"+document.domain+"/fb_login/index.html&scope=offline_access,manage_pages,ads_management,read_insights,publish_actions",
+  "domain": "http://"+document.domain,
+  "api_prefix": "http://"+document.domain+"/fmpapi1.0",
+  "client_id": "798558363555177",
+  "logged_gourl": "http://"+document.domain+"/settings",
+  "redirect_url": "http://"+document.domain+"/redirect/",
+  "fb_login_url": "https://www.facebook.com/v2.2/dialog/oauth?client_id=798558363555177&redirect_uri=http://"+document.domain+"/fb_login/index.html&scope=offline_access,manage_pages,ads_management,read_insights,publish_actions",
   "product_multi_max": 5
 };
+$( document ).ajaxComplete(function( event, xhr, settings ) {
+    // 全局ajax完成触发检查超时登出
+    if (xhr.status===400) {
+      $("body").hide()
+      location.href=baseConf.redirect_url+"not_login.html"
+    }
+});
